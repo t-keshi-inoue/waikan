1518cf1cb5bc737dd76332bdfb8130836365d484
inouejoushi <takeshitoras.79@gmail.com>
2019-11-28 13:49:10 +0900

    feat: validation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,83 +1,112 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../layout/Layout";
 import { Button, FormField, TextInput, Select, Box, Text } from "grommet";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Error from "../layout/Error";
+
+const ValidationSchema = Yup.object().shape({
+  bill: Yup.number()
+    .min(2, "金額が少なすぎます!")
+    .required("金額を入力してください"),
+  numOfPeople: Yup.number()
+    .min(2, "人数が少なすぎます!")
+    .max(1000, "人数が多すぎます!")
+    .required("人数を入力してください"),
+  calcOption: Yup.string().required("塩梅を選択してください"),
+});
 
 const Home = () => {
   return (
     <Layout>
       <Formik
-        validate={values => {
-          const errors = {};
-          if (!values.people) {
-            errors.people = "required";
-          }
-          if (!values.fraction) {
-            errors.fraction = "required";
-          } else if (!values.bill.match(/^[0-9]+$/)) {
-            errors.bill = "numeric only";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
+        initialValues={{ bill: "", numOfPeople: "", calcOption: "" }}
+        validationSchema={ValidationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
+            resetForm();
             setSubmitting(false);
           }, 400);
         }}>
-        {({ values, errors, handleChange, handleSubmit, setFieldValue }) => (
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleSubmit,
+          handleBlur,
+          isSubmitting,
+          setFieldValue,
+        }) => (
           <form onSubmit={handleSubmit}>
-            <Box margin={{ vertical: "small" }}>
-              <FormField label="people">
-                <TextInput
-                  name="people"
-                  placeholder="ex.) 20"
-                  onChange={handleChange}
-                  errors={errors.people}
-                />
-              </FormField>
-              <Text textAlign="end" margin={{ top: "-40px" }}>
-                人
-              </Text>
-            </Box>
             <Box margin={{ vertical: "small" }}>
               <FormField label="お会計">
                 <TextInput
+                  type="number"
                   name="bill"
                   placeholder="ex.) 20000"
                   onChange={handleChange}
-                  error={errors.bill}
+                  onBlur={handleBlur}
+                  value={values.bill}
+                  className={touched.bill && errors.bill ? "has-error" : null}
                 />
               </FormField>
               <Text textAlign="end" margin={{ top: "-40px" }}>
                 円
               </Text>
+              <Error touched={touched.bill} message={errors.bill} />
             </Box>
             <Box margin={{ vertical: "small" }}>
-              <FormField
-                label="塩梅"
-                onChange={handleChange}
-                error={errors.fraction}>
-                <Select
-                  options={["セコ勘", "ダサ勘", "神勘"]}
-                  message="eee"
-                  placeholder="端数はあなたの匙加減..."
+              <FormField label="人数">
+                <TextInput
+                  type="number"
+                  name="numOfPeople"
+                  placeholder="ex.) 20"
+                  onBlur={handleBlur}
+                  value={values.numOfPeople}
+                  onChange={handleChange}
+                  className={
+                    touched.numOfPeople && errors.numOfPeople
+                      ? "has-error"
+                      : null
+                  }
                 />
               </FormField>
+              <Text textAlign="end" margin={{ top: "-40px" }}>
+                人
+              </Text>
+              <Error
+                touched={touched.numOfPeople}
+                message={errors.numOfPeople}
+              />
+            </Box>
+            <Box margin={{ vertical: "small" }}>
+              <FormField label="塩梅">
+                <Select
+                  type="string"
+                  name="calcOption"
+                  onChange={event => setFieldValue("calcOption", event.value)}
+                  options={["セコ勘", "ダサ勘", "神勘"]}
+                  value={values.calcOption}
+                  className={
+                    touched.calcOption && errors.calcOption ? "has-error" : null
+                  }
+                />
+              </FormField>
+              <Error touched={touched.calcOption} message={errors.calcOption} />
             </Box>
 
-            <Form>
-              <Box margin={{ top: "medium", bottom: "small" }}>
-                <ErrorMessage name="email" component="div" />
-                <ErrorMessage name="password" component="div" />
-                <Button
-                  primary
-                  label={<Box align="center">計算する</Box>}
-                  type="submit"
-                  href="./result"
-                />
-              </Box>
-            </Form>
+            <Box margin={{ top: "medium", bottom: "small" }}>
+              <Button
+                primary
+                label={<Box align="center">計算する</Box>}
+                type="submit"
+                // href="./result"
+                disabled={isSubmitting}
+              />
+            </Box>
           </form>
         )}
       </Formik>
